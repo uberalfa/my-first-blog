b18b4a788a01d69290550401c4fe0dd488fffbe5
Kristina <kristina.nevolnikova@gmail.com>
2017-03-12 10:16:47 +0100

    Add aloowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0rnuo3^aed0y$*etlmk*vr!6#w59u0y3ifjf50p1kl!j*oc!az'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
